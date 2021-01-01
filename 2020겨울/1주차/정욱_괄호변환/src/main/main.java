bf4c542320d25eafcd686e305b22478d5b17de89
fnai <dnrlalth@gmail.com>
2021-01-01 10:31:51 +0900

    [괄호변환]1차저장

---
+++ 2020겨울/1주차/정욱_괄호변환/src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class main {
+
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		
+		System.out.println(test.solution("(()())()"));
+		System.out.println(test.solution(")("));
+		System.out.println(test.solution("()))((()"));
+		System.out.println(test.solution(")()()()("));
+	}
+
+}
