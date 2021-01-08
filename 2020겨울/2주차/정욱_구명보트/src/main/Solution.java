9fc05835a9918ea15b252f837855b3c527699f1f
fnai <dnrlalth@gmail.com>
2021-01-08 21:06:17 +0900

    [구명보트]시간복잡도 n제곱으로 풀은것

--- 2020겨울/2주차/정욱_구명보트/src/main/Solution.java
+++ 2020겨울/2주차/정욱_구명보트/src/main/Solution.java
@@ -1,8 +1,36 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Solution {
 	public int solution(int[] people, int limit) {
         int answer = 0;
+        int weight = 0;
+        
+        ArrayList<Integer> sortPeople = new ArrayList<>();
+        
+        for(int sur = 0; sur < people.length ; sur++){
+        	sortPeople.add(people[sur]);
+        }
+        
+        sortPeople.sort(Collections.reverseOrder());
+        
+        for(int sur = 0 ; sur < sortPeople.size() ; sur++){
+        	weight = sortPeople.get(0);
+        	sortPeople.remove(0);
+        	sur--;
+        	
+        	for(int roop = 0 ; roop < sortPeople.size() ; roop++){
+        		if((weight + sortPeople.get(roop)) <= limit){
+        			sortPeople.remove(roop);
+        			
+        			break;
+        		}
+        	}
+        	answer++;
+        }
+        
         return answer;
     }
 }
