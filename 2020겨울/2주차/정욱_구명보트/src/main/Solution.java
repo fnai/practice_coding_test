4e707da415ab1ec0be8a0d67147290d4444bd9b0
fnai <dnrlalth@gmail.com>
2021-01-08 21:38:06 +0900

    [구명보트] 241n으로 줄인코드(프로그래머스 효율성 통과)

--- 2020겨울/2주차/정욱_구명보트/src/main/Solution.java
+++ 2020겨울/2주차/정욱_구명보트/src/main/Solution.java
@@ -4,31 +4,65 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Solution {
+	//시간복잡도가 n제곱인것으로 풀음
+//	public int solution(int[] people, int limit) {
+//        int answer = 0;
+//        int weight = 0;
+//        
+//        ArrayList<Integer> sortPeople = new ArrayList<>();
+//        
+//        for(int sur = 0; sur < people.length ; sur++){
+//        	sortPeople.add(people[sur]);
+//        }
+//        
+//        sortPeople.sort(Collections.reverseOrder());
+//        
+//        for(int sur = 0 ; sur < sortPeople.size() ; sur++){
+//        	weight = sortPeople.get(0);
+//        	sortPeople.remove(0);
+//        	sur--;
+//        	
+//        	for(int roop = 0 ; roop < sortPeople.size() ; roop++){
+//        		if((weight + sortPeople.get(roop)) <= limit){
+//        			sortPeople.remove(roop);
+//        			
+//        			break;
+//        		}
+//        	}
+//        	answer++;
+//        }
+//        
+//        return answer;
+//    }
+	
+	
+	//시간복잡도가 200n인것으로 풀음
 	public int solution(int[] people, int limit) {
         int answer = 0;
         int weight = 0;
         
-        ArrayList<Integer> sortPeople = new ArrayList<>();
+        int[] hashWeight = new int[241];
         
-        for(int sur = 0; sur < people.length ; sur++){
-        	sortPeople.add(people[sur]);
+        for(int sur = 0 ; sur < people.length ; sur++){
+        	hashWeight[people[sur]] += 1;
         }
         
-        sortPeople.sort(Collections.reverseOrder());
-        
-        for(int sur = 0 ; sur < sortPeople.size() ; sur++){
-        	weight = sortPeople.get(0);
-        	sortPeople.remove(0);
-        	sur--;
-        	
-        	for(int roop = 0 ; roop < sortPeople.size() ; roop++){
-        		if((weight + sortPeople.get(roop)) <= limit){
-        			sortPeople.remove(roop);
-        			
-        			break;
+        for(int sur = 240 ; sur > 0 ; sur--){
+        	if(hashWeight[sur] > 0) {
+        		weight = sur;
+        		hashWeight[sur] -= 1;
+        		
+        		for(int roop = (limit - weight); roop > 0 ; roop--){
+        			if(hashWeight[roop] > 0){
+        				hashWeight[roop] -= 1;
+        				
+        				break;
+        			}
         		}
-        	}
-        	answer++;
+        		answer++;
+        		
+        		sur = 240;
+			}
         }
         
         return answer;
--- 2020겨울/2주차/정욱_구명보트/src/main/main.java
+++ 2020겨울/2주차/정욱_구명보트/src/main/main.java
@@ -10,6 +10,15 @@ public class main {
 		
 		int[] input2 = {70, 50, 80};
 		System.out.println(test.solution(input2, 100) + " = 3");
+		
+		int[] input3 = {40, 40, 40, 40, 40, 40};
+		System.out.println(test.solution(input3, 120) + " = 3");
+		
+		int[] input4 = {240};
+		System.out.println(test.solution(input4, 240) + " = 1");
+		
+		int[] input5 = {240, 230, 200, 100, 140, 150, 40, 20, 10};
+		System.out.println(test.solution(input5, 240) + " = 5");
 	}
 
 }
