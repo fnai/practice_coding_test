21000434991a7c5eb6ed1d4427f3017b2f048ede
fnai <dnrlalth@gmail.com>
2021-01-21 13:52:00 +0900

    중간저장. 시간복잡도를 더 줄일 방법을 생각하고 나중에 다시 수정

--- 2020겨울/4주차/정욱_가장큰수/src/main/Solution.java
+++ 2020겨울/4주차/정욱_가장큰수/src/main/Solution.java
@@ -2,42 +2,180 @@ package main;
 
 import java.util.ArrayList;
 
-public class Solution {
-	//첫 숫자로 먼저 1~9까지 배분을 하고 이후에  정렬 로직을 사용하여 큰 수 찾기
+public class Solution {	
+//	//1,3,5,6번 시가노과
+//	public String solution(int[] numbers) {
+//        String answer = "";
+//        int check = 0;
+//        
+//        ArrayList<String> numString = new ArrayList<>();
+//        ArrayList<String> numArray = new ArrayList<>();
+//        
+//        for(int sur = 0 ; sur < numbers.length ; sur++){
+//        	numString.add(Integer.toString(numbers[sur]));
+//      	}
+//        
+//        numArray.add(numString.get(0));
+//        
+//        for(int sur = 1 ; sur < numString.size() ; sur++){
+//        	check = 0;
+//        	
+//        	for(int roop = 0 ; roop < numArray.size() ; roop++){
+//        		if(check == 1){
+//        			check = 0;
+//        			break;
+//        		}
+//        		
+//        		//마지막위치인경우 start-----------------
+//        		if(roop == numArray.size() - 1){
+//        			if(numString.get(sur).length() > numArray.get(roop).length()){
+//        				
+//        				for(int target = 0 ; target < 2 * numString.get(sur).length() ; target++){
+//        					
+//        					if(numString.get(sur).charAt(target%numString.get(sur).length()) == numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//        						if(target == (2*numString.get(sur).length()) - 1){
+//        							numArray.add(roop, numString.get(sur));
+//        							
+//        							check = 1;
+//        							break;
+//        						}
+//        					}
+//        					else if(numString.get(sur).charAt(target%numString.get(sur).length()) > numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//        						numArray.add(roop, numString.get(sur));
+//
+//        						check = 1;
+//        						break;
+//        					}
+//        					else{
+//        						numArray.add(roop + 1, numString.get(sur));
+//        						
+//        						check = 1;
+//        						break;
+//        					}
+//        				}
+//        			}
+//        			
+//        			else{
+//        				for(int target = 0 ; target < 2 * numArray.get(roop).length() ; target++){
+//        					if(numString.get(sur).charAt(target%numString.get(sur).length()) == numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//        						if(target == (2*numArray.get(roop).length()) - 1){
+//        							numArray.add(roop, numString.get(sur));
+//        							
+//        							check = 1;
+//        							break;
+//        						}
+//        					}
+//        					else if(numString.get(sur).charAt(target%numString.get(sur).length()) > numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//        						numArray.add(roop, numString.get(sur));
+//        						
+//        						check = 1;
+//        						break;
+//        					}
+//        					else{
+//        						numArray.add(roop + 1, numString.get(sur));
+//        						
+//        						check = 1;
+//        						break;
+//        					}
+//        				}
+//        			}
+//        			break;
+//        		}
+//        		//마지막위치인경우 end-----------------------
+//        		
+//        		if(numString.get(sur).length() > numArray.get(roop).length()){
+//    				
+//        			for(int target = 0 ; target < 2 * numString.get(sur).length() ; target++){
+//        				if(numString.get(sur).charAt(target%numString.get(sur).length()) == numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//    						if(target == (2*numString.get(sur).length()) - 1){
+//    							numArray.add(roop, numString.get(sur));
+//    							
+//    							check = 1;
+//    							break;
+//    						}
+//    					}
+//    					else if(numString.get(sur).charAt(target%numString.get(sur).length()) > numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//    						numArray.add(roop, numString.get(sur));
+//    						
+//    						check = 1;
+//    						break;
+//    					}
+//    					else{
+//    						break;
+//    					}
+//    				}
+//    			}
+//    			
+//    			else{
+//    				for(int target = 0 ; target < 2 * numArray.get(roop).length() ; target++){
+//    					if(numString.get(sur).charAt(target%numString.get(sur).length()) == numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//    						if(target == (2*numArray.get(roop).length()) - 1){
+//    							numArray.add(roop, numString.get(sur));
+//    							
+//    							check = 1;
+//    							break;
+//    						}
+//    					}
+//    					else if(numString.get(sur).charAt(target%numString.get(sur).length()) > numArray.get(roop).charAt(target%numArray.get(roop).length())){
+//    						numArray.add(roop, numString.get(sur));
+//    						
+//    						check = 1;
+//    						break;
+//    					}
+//    					else{
+//    						break;
+//    					}
+//    				}
+//    			}
+//        	}
+//        }
+//        
+//        for(int sur = 0 ; sur < numArray.size() ; sur++){
+//    		answer += numArray.get(sur);
+//    	}
+//        
+//        if(numArray.get(0).equals("0")){
+//        	return "0";
+//        }
+//        return answer;
+//    }
+	
+	//1,2,3,5,6 시간초과
 	public String solution(int[] numbers) {
         String answer = "";
         
-        ArrayList<String> numString = new ArrayList<>();
+        int max = 0;
+        int check = 0;
         
-        ArrayList<String>[] numArray = new ArrayList[9];
+        ArrayList<String> numArray = new ArrayList<>();
         
-        for(int sur = 0 ; sur < 9 ; sur++){
-        	numArray[sur] = new ArrayList<>();
-        }
+        numArray.add(Integer.toString(numbers[0]));
         
-        for(int sur = 0 ; sur < numbers.length ; sur++){
-        	numString.add(Integer.toString(numbers[sur]));
-        }
-        
-        for(int sur = 0 ; sur < numString.size() ; sur++){
-        	System.out.println(numString.get(sur).charAt(0) - 49 +", "+ numString.get(sur).charAt(0));
-        	numArray[numString.get(sur).charAt(0) - 49].add(numString.get(sur));
-        }
-        
-        for(int sur = 8 ; sur >= 0 ; sur--){
-        	if(numArray[sur].isEmpty()){
-        		continue;
+        for(int sur = 1 ; sur < numbers.length ; sur++){
+        	check = 0;
+        	
+        	for(int target = 0 ; target < numArray.size() ; target++){
+        		if(Integer.parseInt(numArray.get(target)+Integer.toString(numbers[sur])) <= Integer.parseInt(Integer.toString(numbers[sur])+numArray.get(target))){
+        			numArray.add(target, Integer.toString(numbers[sur]));
+        			
+        			break;
+        		}
+        		check++;
         	}
         	
-        	//정렬하는 로직이 들어가야함;;
-        	//----------------
-        	//----------------
-        	
-        	for(int roop = 0 ; roop < numArray[sur].size() ; roop++){
-        		answer += numArray[sur].get(roop);
-        	}
+        	if (check == numArray.size()){
+				numArray.add(check, Integer.toString(numbers[sur]));
+			}
         }
         
+        for(int sur = 0 ; sur < numArray.size() ; sur++){
+    		answer += numArray.get(sur);
+    	}
+        
+        if(numArray.get(0).equals("0")){
+        	return "0";
+        }
+        	
         return answer;
     }
 }
--- 2020겨울/4주차/정욱_가장큰수/src/main/main.java
+++ 2020겨울/4주차/정욱_가장큰수/src/main/main.java
@@ -13,6 +13,52 @@ public class main {
 		int[] input2 = {3, 30, 34, 5, 9};
 		System.out.println(test.solution(input2)+" = 9534330");
 		System.out.println("----------------------------------------------");
+		
+		int[] input3 = {0, 5, 10, 15, 20};
+		System.out.println(test.solution(input3)+" = 52015100");
+		System.out.println("----------------------------------------------");
+		
+		int[] input4 = {1000, 0, 5, 99 ,100};
+		System.out.println(test.solution(input4)+" = 99510010000");
+		System.out.println("----------------------------------------------");
+		
+		int[] input5 = {0,0,0,0,0};
+		System.out.println(test.solution(input5)+" = 0");
+		System.out.println("----------------------------------------------");
+		
+		int[] input6 = {40,403};
+		System.out.println(test.solution(input6)+" = 40403");
+		System.out.println("----------------------------------------------");
+		
+		int[] input7 = {2, 22};
+		System.out.println(test.solution(input7)+" = 222");
+		System.out.println("----------------------------------------------");
+		
+		int[] input8 = {0,0,0,100};
+		System.out.println(test.solution(input8)+" = 100000");
+		System.out.println("----------------------------------------------");
+		
+		int[] input9 = {1, 2, 3, 4, 5, 66};
+		System.out.println(test.solution(input9)+" = 6654321");
+		System.out.println("----------------------------------------------");
+		
+		int[] input10 = {1, 2, 3, 4, 5, 44};
+		System.out.println(test.solution(input10)+" = 5444321");
+		System.out.println("----------------------------------------------");
+		
+		int[] input11 = {40, 403};
+		System.out.println(test.solution(input11)+" = 40403");
+		System.out.println("----------------------------------------------");
+		int[] input12 = {40, 404};
+		System.out.println(test.solution(input12)+" = 40440");
+		System.out.println("----------------------------------------------");
+		int[] input13 = {40, 405};
+		System.out.println(test.solution(input13)+" = 40540");
+		System.out.println("----------------------------------------------");
+		
+		int[] input14 = {12, 121};
+		System.out.println(test.solution(input14)+" = 12121");
+		System.out.println("----------------------------------------------");
 	}
 
 }
