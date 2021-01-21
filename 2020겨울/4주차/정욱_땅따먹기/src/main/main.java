38d717e02295d23a1ccacaaebf16c80be06b3c59
fnai <dnrlalth@gmail.com>
2021-01-21 16:27:07 +0900

    [땅따먹기]시작

---
+++ 2020겨울/4주차/정욱_땅따먹기/src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class main {
+
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		
+		int[][] input = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+		System.out.println(test.solution(input) + " = 16");
+	}
+
+}
